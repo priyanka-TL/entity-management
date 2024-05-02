b913c84821696ab1c0c85cf56e8b12157e500b3a
vishnu <vishnu@tunerlabs.com>
2024-05-02 06:33:42 +0530

    log added for mongo issue debug

--- src/config/db/mongodb.js
+++ src/config/db/mongodb.js
@@ -6,84 +6,78 @@
  */
 
 // dependencies
-const mongoose = require("mongoose")
-const mongoose_delete = require("mongoose-delete")
-const mongoose_autopopulate = require("mongoose-autopopulate")
-const mongoose_timestamp = require("mongoose-timestamp")
-const mongoose_ttl = require("mongoose-ttl")
+const mongoose = require('mongoose')
+const mongoose_delete = require('mongoose-delete')
+const mongoose_autopopulate = require('mongoose-autopopulate')
+const mongoose_timestamp = require('mongoose-timestamp')
+const mongoose_ttl = require('mongoose-ttl')
 const ObjectId = mongoose.Types.ObjectId
 
 /**
  * Mongodb setup.
  * @method
  * @name DB
-*/
+ */
 
 const DB = function () {
+	const db = mongoose.createConnection(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	console.log('mongodbconnection string : ', process.env.MONGODB_URL)
+	console.log('Mongo startup log :  ', db)
+	db.on('error', console.error.bind(console, 'connection error:'))
+	db.once('open', function () {
+		console.log('Connected to DB')
+	})
 
-  const db = mongoose.createConnection(
-    process.env.MONGODB_URL,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  )
+	const createModel = function (opts) {
+		if (typeof opts.schema.__proto__.instanceOfSchema === 'undefined') {
+			var schema = mongoose.Schema(opts.schema, opts.options)
+		} else {
+			var schema = opts.schema
+		}
 
-  db.on("error", console.error.bind(console, "connection error:"))
-  db.once("open", function () {
-    console.log("Connected to DB")
-  })
+		// apply Plugins
+		schema.plugin(mongoose_timestamp, {
+			createdAt: 'createdAt',
+			updatedAt: 'updatedAt',
+		})
+		schema.plugin(mongoose_autopopulate)
+		schema.plugin(mongoose_delete, { overrideMethods: true, deletedAt: true })
 
-  const createModel = function (opts) {
-    if (typeof opts.schema.__proto__.instanceOfSchema === "undefined") {
-      var schema = mongoose.Schema(opts.schema, opts.options)
-    } else {
-      var schema = opts.schema
-    }
+		// Expire at
+		if (opts.options) {
+			if (opts.options.expireAfterSeconds || opts.options.expireAfterSeconds === 0) {
+				log.debug('Expire Configured for ' + opts.name)
+				schema.plugin(mongoose_ttl, {
+					ttl: opts.options.expireAfterSeconds * 1000,
+				})
+			}
+		}
 
-    // apply Plugins
-    schema.plugin(mongoose_timestamp, {
-      createdAt: "createdAt",
-      updatedAt: "updatedAt"
-    })
-    schema.plugin(mongoose_autopopulate)
-    schema.plugin(mongoose_delete, { overrideMethods: true, deletedAt: true })
+		var model = db.model(opts.name, schema, opts.name)
+		return model
+	}
 
-    // Expire at
-    if (opts.options) {
-      if (
-        opts.options.expireAfterSeconds ||
-        opts.options.expireAfterSeconds === 0
-      ) {
-        log.debug("Expire Configured for " + opts.name)
-        schema.plugin(mongoose_ttl, {
-          ttl: opts.options.expireAfterSeconds * 1000
-        })
-      }
-    }
+	const runCompoundIndex = function (modelName, opts) {
+		if (opts && opts.length > 0) {
+			for (let indexPointer = 0; indexPointer < opts.length; indexPointer++) {
+				let currentIndex = opts[indexPointer]
+				db.collection(modelName).createIndex(currentIndex.name, currentIndex.indexType)
+			}
+		}
+	}
 
-    var model = db.model(opts.name, schema, opts.name)
-    return model
-  }
-
-  const runCompoundIndex = function (modelName, opts) {
-    if (opts && opts.length > 0) {
-      for (let indexPointer = 0; indexPointer < opts.length; indexPointer++) {
-        let currentIndex = opts[indexPointer]
-        db.collection(modelName).createIndex(currentIndex.name, currentIndex.indexType)
-      }
-    }
-  }
-
-  return {
-    database: db,
-    createModel: createModel,
-    ObjectId: ObjectId,
-    models: db.models,
-    runCompoundIndex: runCompoundIndex
-  }
+	return {
+		database: db,
+		createModel: createModel,
+		ObjectId: ObjectId,
+		models: db.models,
+		runCompoundIndex: runCompoundIndex,
+	}
 }
 
 module.exports = DB
