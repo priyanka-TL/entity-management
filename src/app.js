0fdb20c3a04c4c6d15d98c92378ffdb1323e8867
prajwal <prajwal@tunerlabs.com>
2024-11-12 17:53:23 +0530

    file import changes made

--- src/app.js
+++ src/app.js
@@ -5,37 +5,43 @@
  * Description : Root file.
  */
 
+require('module-alias/register')
 require('dotenv').config()
-
 // express
 const express = require('express')
 const app = express()
+const { elevateLog } = require('elevate-logger')
+const logger = elevateLog.init()
 const fileUpload = require('express-fileupload')
-var fs = require('fs')
+const fs = require('fs')
+
 // Health check
 require('./healthCheck')(app)
 
-// Setup application config, establish DB connections and set global constants.
-require('./config/connections')
-require('./config/globals')()
+// Setup application config, establish DB connections, and set global constants.
+require('@config/connections')
+require('@config/globals')()
 
-// Check if all environment variables are provided.
-const environmentData = require('./envVariables')()
+const environmentData = require('./envVariables.js')()
 
 if (!environmentData.success) {
-	console.log('Server could not start . Not all environment variable is provided')
+	logger.error('Server could not start . Not all environment variable is provided', {
+		triggerNotification: true,
+	})
 	process.exit()
 }
+// Check if all environment variables are provided.
 
-//required modules
+// Required modules
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 const expressValidator = require('express-validator')
 
-//To enable cors
+// Enable CORS
 app.use(cors())
 app.use(expressValidator())
+
 // Middleware for handling file uploads
 app.use(fileUpload())
 app.use(bodyParser.json({ limit: '50MB' }))
@@ -43,28 +49,30 @@ app.use(bodyParser.urlencoded({ limit: '50MB', extended: false }))
 
 app.use(express.static('public'))
 
+// API Documentation
 app.get(process.env.API_DOC_URL, function (req, res) {
-	res.sendFile(path.join(__dirname, './api-doc/index.html'))
+	res.sendFile(path.resolve('./api-doc/index.html'))
 })
 
+// Debugging middleware
 app.all('*', (req, res, next) => {
 	console.log({ 'Debugging Entity Service': true })
 	console.log('<------------Request log starts here------------------>')
 	console.log('Request URL: ', req.url)
 	console.log('Request Headers: ', JSON.stringify(req.headers))
 	console.log('Request Body: ', JSON.stringify(req.body))
-	console.log('Request Files: ', req.files)
+	//   console.log("Request Files: ", req.files)
 	console.log('<---------------Request log ends here------------------>')
 	next()
 })
 
 // Router module
-const router = require('./routes')
+const router = require('@routes')
 
-//add routing
+// Add routing
 router(app)
 
-//listen to given port
+// Listen on the specified port
 app.listen(process.env.APPLICATION_PORT, () => {
 	console.log('Environment : ' + process.env.APPLICATION_ENV)
 	console.log('Application is running on the port : ' + process.env.APPLICATION_PORT)
